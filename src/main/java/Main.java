3ae0287296da298441e478087450b8921fb5f296
Nikolay Pelov <nikolai_pelov@abv.bg>
2018-06-17 17:11:43 +0200

    Added j soup as a local dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,16 @@
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Crawler crawler = new Crawler("http://localhost/crawl/catalog.php","http://localhost/crawl/");
-
-        System.out.println(crawler.getLink("books"));
-        System.out.println(crawler.getLink("movies"));
-        System.out.println(crawler.getLink("music"));
-        String music_link = crawler.getLink("music");
+        Crawler crawler = new Crawler("http://i349425.hera.fhict.nl/catalog.php","http://i349425.hera.fhict.nl/");
+        MovieCrawler movieCrawler = new MovieCrawler("http://i349425.hera.fhict.nl/");
+        List<Movie> movies = movieCrawler.getAllMovies("http://i349425.hera.fhict.nl/");
+        System.out.println(movies);
+//        System.out.println(crawler.getLink("books"));
+//        System.out.println(crawler.getLink("movies"));
+//        System.out.println(crawler.getLink("music"));
+//        String music_link = crawler.getLink("music");
 
 //        Crawler music_crawler = new MusicCrawler(music_link,"http://localhost/simple_site/");
 //
--- src/main/java/MovieCrawler.java
+++ src/main/java/MovieCrawler.java
@@ -1,3 +1,96 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.*;
+
 public class MovieCrawler {
+    public  String root;
+    public Set<String> pagesCrawled = new HashSet<String>();
+    public List<String> pagesToCrawl = new LinkedList<String>();
+    private List<Movie> moviesFound = new ArrayList<>();
+
+    public MovieCrawler(String url) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("URL does not exist");
+        }
+        root = doc.baseUri();
+    }
+
+    public List<String> getLinksFromPage(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.getElementsByTag("a");
+        for (Element link : links) {
+            String linkHref = link.attr("abs:href");
+            System.out.println("Parse" + linkHref);
+            this.addPageToCrawl(linkHref);
+        }
+        return pagesToCrawl;
+    }
+
+    public void addPageToCrawl(String linkHref) {
+        linkHref = linkHref.toLowerCase();
+        if (!linkHref.contains(root)) return;
+        if (pagesToCrawl.contains(linkHref) || pagesCrawled.contains(linkHref)) return;
+        pagesToCrawl.add(linkHref);
+        System.out.println("Page added to list: " + linkHref);
+    }
+
+    public void addVisitedPage(String linkHref) {
+        linkHref = linkHref.toLowerCase();
+        if (!linkHref.contains(root)) return;
+        pagesCrawled.add(linkHref);
+    }
+
+    public Movie searchMovie(String url) throws IOException {
+        Movie movie;
+        String genre = "";
+        String format = "";
+        String year = "";
+        String director = "";
+        List<String> writers = new ArrayList<>();
+        List<String> stars = new ArrayList<>();
+        Document doc = Jsoup.connect(url).get();
+        Element content = doc.getElementById("content");
+        if (url.contains("id=")){
+            Element details = content.select("table").first();
+            Elements rows = details.getElementsByTag("tr");
+            for (Element row : rows) {
+                Element header = row.getElementsByTag("th").first();
+                Element data = row.getElementsByTag("td").first();
+                if (header.text().contains("Genre")) genre = data.text();
+                if (header.text().contains("Format")) format = data.text();
+                if (header.text().contains("Year")) year = data.text();
+                if (header.text().contains("Director")) director = data.text();
+                if (header.text().contains("Writers")) writers = Arrays.asList(data.text().split(","));
+                if (header.text().contains("Stars")) stars = Arrays.asList(data.text().split(","));
+            }
+        }
+        try {
+            movie = new Movie(genre,format,Integer.parseInt(year),director,writers,stars);
+        } catch (Exception e) {
+            movie = null;
+        }
+        return movie;
+    }
+
+    public void parseURL(String url) throws IOException {
+        this.addVisitedPage(url);
+        this.getLinksFromPage(url);
+        Movie movie = this.searchMovie(url);
+        if (movie != null) moviesFound.add(movie);
+        if (this.pagesToCrawl.isEmpty()) return ;
+        this.parseURL(this.pagesToCrawl.remove(0));
+    }
+
+    public List<Movie> getAllMovies(String url) throws IOException {
+        this.parseURL(url);
+        return this.moviesFound;
+    }
 
 }
