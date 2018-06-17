8fcb0d18b4c14a88f91611b86817f702138a3a10
Nikolay Pelov <nikolai_pelov@abv.bg>
2018-06-17 16:13:34 +0200

    Created Test Structure for movie crawler

--- src/test/java/MoviesCrawlerTest.java
+++ src/test/java/MoviesCrawlerTest.java
@@ -1,2 +1,75 @@
+import org.junit.Test;
+
 public class MoviesCrawlerTest {
+    private String rootURI = "http://i349425.hera.fhict.nl/";
+    private Integer numberOfLinkOnBaseURI = 8;
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldSetBaseURI(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionWithWrongURI(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldRetrieveLinks(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldIgnoreNonDomainLinks(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldAddPageToCrawl(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldAddVisitedPages(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotFindAMovie(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFindABookAndNotRetrieveIt(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldCallAddVisitedPage(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldCallGetLinks(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldCallFindMovie(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldAddMovieIfFound(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldBreakWhenNoPagesLeft(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldReturnAllMoviesOnAWebsite(){
+
+    }
 }
